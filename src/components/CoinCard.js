0862218560f5aee7a04ec7eb376e8666da7508f0
Christopher Yeo <chris.yeosj@gmail.com>
2018-07-24 00:32:31 +0800

    added CoinCard and Improved CryptoContainer

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -1,14 +1,37 @@
 import react from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, image} from 'react-native';
 
-const CoinCard = (props) => {
+import {images} from '../Utils/CoinIcons';
+
+const styles=StyleSheet.create({
+  container:{
+    display:'flex',
+  },
+  image:{
+    width: 40,
+    height: 40,
+  },
+  bold:{
+    fontWeight: 'bold',
+  },
+})
+
+const {container, image, bold} = styles;
+
+const CoinCard = ({symbol, coin_name, price_usd, percent_change_24h, percent_change_7d}) => {
   return (
-    <View>
-      <Text>{props.symbol}</Text>
-      <Text>{props.price_usd}</Text>
-      <Text>{props.percent_change_24h}</Text>
+    <View style={container}>
+      <Image
+        style={styles.image}
+        source={{uri:images[symbol]}}
+      />
+      <Text>{symbol}</Text>
+      <Text>{coin_name}</Text>
+      <Text><Text style={bold}>$</Text>{price_usd}</Text>
+      <Text>Change past 24 hours: {percent_change_24h}</Text>
+      <Text>Change past 7 days: {percent_change_7d}</Text>
     </View>
-  )
+  );
 }
 
 export default CoinCard;
--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { View, Text} from 'react-native';
-
+import { View, Text, ScrollView} from 'react-native';
+import CoinCard from './CoinCard';
 import FetchCoinData from '../../Actions/FetchCoinData';
+
+import Spinner from 'react-native-loading-spinner-overlay';
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingBottom:100,
+    paddingTop: 55,
+  }
+})
 
 class CryptoContainer extends Component{
 
-  componentDidMount(){
+  componentWillMount(){
     this.props.FetchCoinData();
   }
 
+  _renderCoinCards(){
+    const {crypto} = this.props;
+    return crypto.data.map((coin, index) => {
+      <CoinCard
+        key={index}
+        coin_name={coin.name}
+        symbol={coin.symbol}
+        price_usd={coin.price_usd}
+        percent_change_24h={coin.percent_change_24h}
+        percent_change_7d={coin.percent_change_7d}
+    })
+  }
+
   render(){
+    const {crypto} = this.props;
+
+    if(crypto.isFetching){
+      return (<View>
+                  <Spinner
+                      visible={crpyto.isFetching}
+                      textContent={'Loading..'}
+                      textStyle={{color: '#253145'}}
+                      animation='fade'
+                  />
+              </View>
+            )
+    }
+
     return (
-      <View>
-        <Text>Container</Text>
-      </View>
+      <ScrollView contentContainerStyle={contentContainer}>
+          {_renderCoinCards()}
+      </ScrollView>
     )
   }
 }
