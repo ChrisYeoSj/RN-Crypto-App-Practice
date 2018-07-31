614b011ae043a140d705f1278a03d5531aadb9eb
Christopher Yeo <chris.yeosj@gmail.com>
2018-07-31 22:18:45 +0800

    Beautify CoinCard - wip

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -26,12 +26,12 @@ const styles=StyleSheet.create({
     marginBottom: 10,
     display:'flex',
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
   },
   lowerTier:{
     display:'flex',
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
     marginBottom: 20,
   },
   red:{
@@ -39,29 +39,53 @@ const styles=StyleSheet.create({
   },
   blue:{
     color: '#0000ff',
+  },
+  imageContainer:{
+    flex: 1,
+    paddingLeft:10,
+  },
+  symbolContainer:{
+    flex: 2
+  },
+  priceContainer:{
+    flex: 2
+  },
+  day:{
+    flex:1,
+    textAlign: 'center',
+  },
+  week:{
+    flex:1,
+    textAlign: 'center',
   }
 })
 
-const {container, image, bold} = styles;
+const {container, image, bold, divider, upperTier, imageContainer, lowerTier, red, blue, priceContainer, symbolContainer, day, week} = styles;
 
 const CoinCard = ({symbol, coin_name, price_usd, percent_change_24h, percent_change_7d}) => {
 
   return (
     <View style={container}>
-      <View style={styles.divider}></View>
-      <View style={styles.upperTier}>
-        <Image
-          style={styles.image}
-          source={{uri:coinImages[symbol]}}
-        />
-        <Text><Text style={bold}>{symbol}</Text> | {coin_name}</Text>
-        <Text><Text style={bold}>$</Text>{price_usd}</Text>
+      <View style={divider}></View>
+      <View style={upperTier}>
+        <View style={imageContainer}>
+          <Image
+            style={image}
+            source={{uri:coinImages[symbol]}}
+          />
+        </View>
+        <View style={symbolContainer}>
+          <Text><Text style={bold}>{symbol}</Text> | {coin_name}</Text>
+        </View>
+        <View style={priceContainer}>
+          <Text><Text style={bold}>$</Text>{price_usd}</Text>
+        </View>
       </View>
-      <View style={styles.lowerTier}>
-        <Text>24h: <Text style={[percent_change_24h < 0 ? styles.red:styles.blue]}>{percent_change_24h}</Text></Text>
-        <Text>7d: <Text style={[percent_change_7d < 0 ? styles.red:styles.blue]}>{percent_change_7d}</Text></Text>
+      <View style={lowerTier}>
+        <Text style={day}>24h: <Text style={[percent_change_24h < 0 ? red:blue]}>{percent_change_24h}</Text></Text>
+        <Text style={week}>7d: <Text style={[percent_change_7d < 0 ? red:blue]}>{percent_change_7d}</Text></Text>
       </View>
-      <View style={styles.divider}></View>
+      <View style={divider}></View>
     </View>
   );
 }
